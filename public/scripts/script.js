3fa6d2423235e9742f001e44501ea2248a65b2e8
reubenmilag <reuben.milag@gmail.com>
2020-08-20 16:49:28 +0530

    Added blogs main page and fixed bugs

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -8,3 +8,17 @@ $(".custom-file-input").on("change", function() {
     var fileName = $(this).val().split("\\").pop();
     $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
 });
+
+
+/*  Blogs.sort(function(a, b){return b - a}).reverse(); */
+/* 
+function sortBy(sortingOrder) {
+    if (sortingOrder = 'asc') {
+        document.getElementById('SortingDesc').style.display = "none";
+        document.getElementById('SortingAsc').style.display = "inline-block";
+    }
+    if (sortingOrder = 'desc') {
+        document.getElementById('SortingDesc').style.display= "inline-block";
+        document.getElementById('SortingAsc').style.display = "none";
+    }
+} */
--- routes/routeHandling.js
+++ routes/routeHandling.js
@@ -16,6 +16,103 @@ router.get('/', async(req, res) => {
     }
 })
 
+//BLOGS PAGE
+router.get('/blogs:sort', async (req, res) => {
+    let blogPageQuery = blogs.find().sort({blog_uploadDate: 'desc'})
+    let blogPageQueryAsc = blogs.find().sort({blog_uploadDate: 'asc'})
+    let blogPageQueryNutrition = blogs.find().where('blog_primaryTag').equals('Nutrition').sort({blog_uploadDate: 'asc'})
+    let blogPageQueryEvents = blogs.find().where('blog_primaryTag').equals('Events').sort({blog_uploadDate: 'desc'})
+    let blogPageQueryRecipes = blogs.find().where('blog_primaryTag').equals('Recipes').sort({blog_uploadDate: 'desc'})
+    let blogPageQueryLifestyle = blogs.find().where('blog_primaryTag').equals('Lifestyle').sort({blog_uploadDate: 'desc'})
+
+    if (req.params.sort == ':asc') {
+        try {
+            Blogs = await blogPageQueryAsc.exec() 
+            res.render('blogs', {
+                Blogs: Blogs
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    } 
+
+    else if (req.params.sort == ':nutrition') {
+        try {
+            Blogs = await blogPageQueryNutrition.exec() 
+            res.render('blogs', {
+                Blogs: Blogs
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    else if (req.params.sort == ':events') {
+        try {
+            Blogs = await blogPageQueryEvents.exec() 
+            res.render('blogs', {
+                Blogs: Blogs
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    else if (req.params.sort == ':recipes') {
+        try {
+            Blogs = await blogPageQueryRecipes.exec() 
+            res.render('blogs', {
+                Blogs: Blogs
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    else if (req.params.sort == ':lifestyle') {
+        try {
+            Blogs = await blogPageQueryLifestyle.exec() 
+            res.render('blogs', {
+                Blogs: Blogs
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
+    else {
+        try {
+            Blogs = await blogPageQuery.exec()
+            res.render('blogs', {
+                Blogs: Blogs
+            })
+        } catch (error) {
+            console.log('error in else catch')
+        }     
+    }
+})
+
+/* router.post('/blogs:met', async (req, res) => {
+    let blogPageQuery = blogs.find().sort({blog_uploadDate: 'desc'})
+    let blogPageQueryAsc = blogs.find().sort({blog_uploadDate: 'asc'})
+    console.log(req.body.sortByAsc)
+    if (req.body.sortByAsc == null || req.body.sortByAsc == '' ) {
+        Blogs = await blogPageQueryAsc.exec()        
+    } 
+    if (req.body.sortByAsc == 'asc') {
+        Blogs = await blogPageQueryAsc.exec() 
+    }
+    try {
+        res.render('blogs', {
+            Blogs: Blogs
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}) */
+
+
+
 //CONTACT PAGE
 router.get('/contact-us', (req, res) => {
     res.render('contact-us')
