37fd50e1f90f6c2a5f60a80121f70fa3b67d7780
reubenmilag <reuben.milag@gmail.com>
2020-10-18 21:14:51 +0530

    bugs fixed

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const { check, validationResult } = require('express-validator');
+const blogs = require('../models/blogModel')
+const { check, validationResult } = require('express-validator')
 const bcrypt = require('bcryptjs')
 const passport = require('passport')
 const nodemailer = require('nodemailer')
@@ -24,6 +25,39 @@ router.get('/logout', async (req, res) => {
     req.logout();
     req.flash('success_msg', 'You are Logged out.')
     res.redirect('/admin/login')
+})
+
+
+
+//ON ADMIN BLOG EDIT REQUEST
+router.get('/blog-editor', async (req, res) => {
+    let blogPageQuery = blogs.find().sort({blog_uploadDate: 'desc'})
+    try {
+        Blogs = await blogPageQuery.exec()
+        res.render('blogs-editor', {
+            Blogs: Blogs
+        })
+    } catch (error) {
+        console.log('error in else catch')
+        res.render('/dashboard')
+    }
+})
+
+
+
+//ON ADMIN BLOG SELECTED FOR EDITING
+router.get('/editor/:blogName', async (req, res) => {
+    let blogPageQuery = blogs.findOne().where('blog_link').equals(req.params.blogName)
+    let Blog = await blogPageQuery.exec() 
+
+    try {
+        res.render('editor', {
+            Blog: Blog
+        })   
+    } catch (error) {
+        console.log(error)
+        res.redirect('/blogs:')
+    }
 })
 
 
